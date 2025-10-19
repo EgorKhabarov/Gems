5e53770de792351feda2c8c656432657f6c7b5c5
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-10-20 01:05:36 +0400

    update

--- src/main/java/egorkhabarov/utils/ChatUtils.java
+++ src/main/java/egorkhabarov/utils/ChatUtils.java
@@ -1,13 +1,24 @@
 package egorkhabarov.utils;
 
+import egorkhabarov.Gems;
+import egorkhabarov.config.ConfigManager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 
 import java.util.Locale;
 
 public class ChatUtils {
-    public static void sendGemsMessage(Player player, int value) {
-        String message = "§aYou received §l" + String.format(Locale.US, "%,d",value) + " Gems";
+    public static void sendWithdrawGemsMessage(Player player, int value) {
+        // String message = "§aYou withdrew §l" + String.format(Locale.US, "%,d",value) + " Gems";
+        ConfigManager config = Gems.getInstance().getConfigManager();
+        String message = String.format(Locale.US, config.getWithdrawMessage(), value);
+        player.sendMessage(message);
+    }
+
+    public static void sendReceivedGemsMessage(Player player, int value) {
+        // String message = "§aYou received §l" + String.format(Locale.US, "%,d",value) + " Gems";
+        ConfigManager config = Gems.getInstance().getConfigManager();
+        String message = String.format(Locale.US, config.getReceivedMessage(), value);
         player.sendMessage(message);
         player.sendActionBar(Component.text(message));
     }
